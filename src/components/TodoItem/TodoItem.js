cc7f0a3d6924bafa5f8a28886d5ee54004336e39
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-09-14 18:28:16 +0900

    first commit

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 const TodoItem = ({ todo, id, deleteItem }) => {
+    const [state, setState] = useState("완료");
+
     const onClick = () => {
         deleteItem(id);
     };
+
+    const updateTodo = (e) => {
+        state === "완료" ? setState("취소") : setState("완료");
+    };
+
     return (
         <div>
-            {id + 1}, {todo}
+            <span style={{ textDecoration: state === "완료" ? "none" : "line-through" }}>
+                {id + 1} {todo}
+            </span>
+            <button onClick={updateTodo}>{state}</button>
             <button onClick={onClick}>삭제</button>
         </div>
     );
